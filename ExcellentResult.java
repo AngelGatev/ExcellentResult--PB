7abc0baec65f6dff34d001d50b9f73834fd149b3
Angel <107347177+AngelGatev@users.noreply.github.com>
2022-07-09 18:30:44 +0300

    Add files via upload

---
+++ ExcellentResult.java
@@ -0,0 +1,15 @@
+package JavaBasics.ConditionalStatements.Lab;
+
+import java.util.Scanner;
+
+public class ExcellentResult {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = Integer.parseInt(scanner.nextLine());
+
+        if (a >= 5) {
+            System.out.println("Excellent!");
+        }
+    }
+}
